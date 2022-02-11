30a241073509364b25ebcb594a28d345342cfa2a
DO VIS <2917097922@qq.com>
2022-02-11 16:49:06 +0800

    把字符串转换成整数

---
+++ nowcoder/3731 把字符串转换成整数/3731.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int StrToInt(string str) {
+        string::iterator it = str.begin();
+        int i = 0;
+        int flag = 1;
+
+        //ÅÐ¶ÏÊ×ÔªËØ
+        if (*it == '+')
+        {
+            it++;
+        }
+        else if (*it == '-')
+        {
+            it++;
+            flag = -1;
+        }
+        else if (*it <= '9' && *it >= '0')
+        {
+
+        }
+        else
+        {
+            return 0;
+        }
+        while (it != str.end())
+        {
+            if (*it > '9' || *it < '0')
+                return 0;
+            i = i * 10 + flag * int(*it - '0');
+            it++;
+        }
+
+        return i;
+    }
+};
