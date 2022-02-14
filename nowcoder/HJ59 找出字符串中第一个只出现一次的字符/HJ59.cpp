d86e03526110688b3fe59807252445fc42db3321
DO VIS <2917097922@qq.com>
2022-02-14 13:43:02 +0800

    HJ59 找出字符串中第一个只出现一次的字符

---
+++ nowcoder/HJ59 找出字符串中第一个只出现一次的字符/HJ59.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    while (getline(cin, s))
+    {
+        int count[256] = { 0 };
+        for (size_t i = 0; i < s.size(); i++)
+        {
+            //左边128个记录对应字符数出现的次数，右128个记录第一次出现的下标
+            size_t _n = s[i];
+            if (count[_n] == 0)
+            {
+                count[_n + 128] = i;
+            }
+            count[_n] += 1;
+        }
+        //从下标开始查找
+        int p = -1;
+        for (size_t i = 0; i < 128; i++)
+        {
+            if (count[i] == 1)
+            {
+                if (p == -1 || p > count[i + 128])
+                {
+                    p = count[i + 128];
+                }
+            }
+        }
+        if (p == -1)
+            cout << -1 << endl;
+        else
+            cout << s[p] << endl;
+    }
+
+    return 0;
+}
