814eb77f287bb63f32e3f1fbf950f7bbda754e3a
DO VIS <2917097922@qq.com>
2022-02-23 12:58:52 +0800

    JZ45 把数组排成最小的数

---
+++ nowcoder/JZ45 把数组排成最小的数/JZ45.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    static bool cmp(int x, int y)
+    {
+        string s1 = to_string(x);
+        string s2 = to_string(y);
+        return s1 + s2 < s2 + s1;
+    }
+    string PrintMinNumber(vector<int> numbers) {
+        //≈≈–ÚÀºœÎ
+        //if(numbers.size() == 0)
+            //return "";
+        size_t pos = 0;
+        /*for(size_t i = 0; i < numbers.size() - 1; i++)
+        {
+            pos = 0;
+            while(pos < numbers.size() - 1 - i)
+            {
+                if(!cmp(numbers[pos], numbers[pos + 1]))
+                {
+                    std::swap(numbers[pos], numbers[pos + 1]);
+                }
+                pos++;
+            }
+        }*/
+        sort(numbers.begin(), numbers.end(), cmp);
+        pos = 0;
+        string s = "";
+        while (pos < numbers.size())
+        {
+            s += to_string(numbers[pos]);
+            pos++;
+        }
+        return s;
+    }
+};
