ea88dd91e929927a0bf33093eafecc32094cfe69
DO VIS <2917097922@qq.com>
2022-03-02 23:31:48 +0800

    JZ69 跳台阶

--- nowcoder/JZ69 跳台阶/JZ69.cpp
+++ nowcoder/JZ69 跳台阶/JZ69.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
-    int jumpFloor(int number) {
-        if (number <= 2)
+    /*int jumpFloor(int number) {
+        if(number <= 2)
             return number;
         return jumpFloor(number - 1) + jumpFloor(number - 2);
+    }*/
+    int jumpFloor(int number)
+    {
+        int fn_1 = 1;
+        int fn_2 = 0;
+        int fn = 0;
+        while (number--)
+        {
+            fn = fn_1 + fn_2;
+            fn_2 = fn_1;
+            fn_1 = fn;
+        }
+        return fn;
     }
+
 };
