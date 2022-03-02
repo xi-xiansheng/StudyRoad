8173bef33c22e7f2ffaa421b952f623a53b855cf
DO VIS <2917097922@qq.com>
2022-03-02 15:31:09 +0000

    删除文件 nowcoder/JZ69 跳台阶

--- nowcoder/JZ69 跳台阶/JZ69.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int jumpFloor(int number) {
-        if (number <= 2)
-            return number;
-        return jumpFloor(number - 1) + jumpFloor(number - 2);
-    }
-};
