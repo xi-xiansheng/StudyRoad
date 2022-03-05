1007248af70f2756c63f318946ea63a39ee163cf
DO VIS <2917097922@qq.com>
2022-03-05 12:21:41 +0800

    JZ41 数据流中的中位数

---
+++ nowcoder/JZ41 数据流中的中位数/JZ41.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    //Ë¼Â·£º¶þ²æËÑË÷Ê÷
+    multiset<int> mst;
+
+    void Insert(int num) {
+        mst.insert(num);
+    }
+    double GetMedian()
+    {
+        std::multiset<int>::iterator it = mst.begin();
+        int i = 0;
+        while (i < mst.size() / 2 + mst.size() % 2)
+        {
+            it++;
+            i++;
+        }
+        double result = double(*--it);
+        if (mst.size() % 2 == 0)
+        {
+            it++;
+            result += double(*it);
+            result /= 2;
+        }
+        return result;
+    }
+};
