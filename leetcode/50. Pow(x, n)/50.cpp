0c4e3c2c54821613ab65528ca6586190726ee8f7
DO VIS <2917097922@qq.com>
2022-04-09 23:59:54 +0800

    50. Pow(x, n)

---
+++ leetcode/50. Pow(x, n)/50.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double quickMul(double x, long long N) {
+        double ans = 1.0;
+        double x_contribute = x;
+        while (N > 0) {
+            if (N % 2 == 1) {
+
+                ans *= x_contribute;
+            }
+            x_contribute *= x_contribute;
+            N /= 2;
+        }
+        return ans;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+};
