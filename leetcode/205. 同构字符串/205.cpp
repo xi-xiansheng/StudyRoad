abc3019942f71f9be0786d877f3f62a9256220ec
DO VIS <2917097922@qq.com>
2022-06-07 20:59:39 +0800

    205. 同构字符串

---
+++ leetcode/205. 同构字符串/205.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int sz = s.size();
+        vector<int> v1(256, -1);
+        vector<int> v2(256, -1);
+        for (int i = 0; i < sz; i++)
+        {
+            if (v1[s[i]] == -1)
+            {
+                v1[s[i]] = t[i];
+            }
+            else
+            {
+                if (v1[s[i]] != t[i])
+                    return false;
+            }
+            if (v2[t[i]] == -1)
+            {
+                v2[t[i]] = s[i];
+            }
+            else
+            {
+                if (v2[t[i]] != s[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
