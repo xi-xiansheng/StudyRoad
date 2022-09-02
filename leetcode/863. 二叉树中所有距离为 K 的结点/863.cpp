d0cdb1ce07bd725c9e910494f4b86490ac44de30
DO VIS <2917097922@qq.com>
2022-09-02 10:57:39 +0800

    863. 二叉树中所有距离为 K 的结点

--- leetcode/863. 二叉树中所有距离为 K 的结点/863.cpp
+++ leetcode/863. 二叉树中所有距离为 K 的结点/863.cpp
@@ -7,54 +7,89 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ /*class Solution {
+ public:
+     int dfs(TreeNode* root, TreeNode* parent)
+     {
+         if(!root)   return 0;
+         int left = dfs(root->left, root);
+         int right = dfs(root->right, root);
+         graph[root->val][parent->val] = 1;
+         graph[parent->val][root->val] = 1;
+         visited[root->val] = false;
+         return 1 + left + right;
+     }
+     vector<int> distanceK(TreeNode* root, TreeNode* target, int _k) {
+         // 图--单源最短路径
+         int n = 1 + dfs(root->left, root) + dfs(root->right, root);
+         visited[target->val] = true;
+         while(--n)
+         {
+             graph[target->val][target->val] = 0x7fffffff;
+             int ret = target->val;
+             for(auto& [k, v] : graph[target->val])
+             {
+                 // 寻找最短路径节点
+                 if(visited[k] == false)
+                 {
+                     if(v < graph[target->val][ret])  ret = k;
+                 }
+             }
+             // 距离松弛
+             for(auto& [k, v] : graph[ret])
+             {
+                 if(graph[target->val].count(k) == 0 ||
+                     graph[target->val][k] > graph[target->val][ret] + graph[ret][k])
+                 {
+                     graph[target->val][k] = graph[target->val][ret] + graph[ret][k];
+                 }
+             }
+             visited[ret] = true;
+         }
+         graph[target->val][target->val] = 0;
+         vector<int> ans;
+         for(auto& [k, v] : graph[target->val])
+         {
+             if(v == _k) ans.push_back(k);
+         }
+         return ans;
+     }
+ private:
+     unordered_map<int, map<int, int>> graph;
+     unordered_map<int, bool> visited;
+ };*/
 class Solution {
 public:
-    int dfs(TreeNode* root, TreeNode* parent)
+    void dfs(TreeNode* root, TreeNode* parent)
     {
-        if (!root)   return 0;
-        int left = dfs(root->left, root);
-        int right = dfs(root->right, root);
-        graph[root->val][parent->val] = 1;
-        graph[parent->val][root->val] = 1;
-        visited[root->val] = false;
-        return 1 + left + right;
+        if (!root)   return;
+        dfs(root->left, root);
+        dfs(root->right, root);
+        p[root] = parent;
+        visited[root] = false;
     }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int _k) {
-        // 图--单源最短路径
-        int n = 1 + dfs(root->left, root) + dfs(root->right, root);
-        visited[target->val] = true;
-        while (--n)
+    void DfsSearch(TreeNode* root, int step, int k)
+    {
+        if (!root)   return;
+        if (visited[root])   return;
+        visited[root] = true;
+        if (step == k)
         {
-            graph[target->val][target->val] = 0x7fffffff;
-            int ret = target->val;
-            for (auto& [k, v] : graph[target->val])
-            {
-                // 寻找最短路径节点
-                if (visited[k] == false)
-                {
-                    if (v < graph[target->val][ret])  ret = k;
-                }
-            }
-            // 距离松弛
-            for (auto& [k, v] : graph[ret])
-            {
-                if (graph[target->val].count(k) == 0 ||
-                    graph[target->val][k] > graph[target->val][ret] + graph[ret][k])
-                {
-                    graph[target->val][k] = graph[target->val][ret] + graph[ret][k];
-                }
-            }
-            visited[ret] = true;
+            ans.push_back(root->val);
+            return;
         }
-        graph[target->val][target->val] = 0;
-        vector<int> ans;
-        for (auto& [k, v] : graph[target->val])
-        {
-            if (v == _k) ans.push_back(k);
-        }
+        DfsSearch(root->left, step + 1, k);
+        DfsSearch(root->right, step + 1, k);
+        DfsSearch(p[root], step + 1, k);
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        dfs(root->left, root);
+        dfs(root->right, root);
+        DfsSearch(target, 0, k);
         return ans;
     }
 private:
-    unordered_map<int, map<int, int>> graph;
-    unordered_map<int, bool> visited;
+    unordered_map<TreeNode*, TreeNode*> p;
+    unordered_map<TreeNode*, bool> visited;
+    vector<int> ans;
 };
